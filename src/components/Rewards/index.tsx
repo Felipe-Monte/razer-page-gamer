89e4599f6502f0e3fc1b5dbfe2ecb63ab55529f6
Felipe <carlosfelipemonte1997@gmail.com>
2025-11-08 11:10:14 -0300

    feat: refactor code

--- src/components/Rewards/index.tsx
+++ src/components/Rewards/index.tsx
@@ -1,33 +1,30 @@
 import { IconDol, IconTrophy, IconUser } from '../../../public/assets/svg';
 
+interface CardItem {
+  icon: React.ReactNode;
+  number: string;
+  title: string;
+}
+
+const cardContent: CardItem[] = [
+  { icon: <IconTrophy />, number: '124', title: 'Campeonatos' },
+  { icon: <IconUser />, number: '2.6M', title: 'Seguidores' },
+  { icon: <IconDol />, number: '14M', title: 'Recompensados' },
+];
+
 export default function Rewards() {
   return (
     <div className=" w-full h-[114px] bg-[#212430] flex items-center justify-around">
-      <div className="flex flex-col items-center justify-center min-w-[100px]">
-        <span>
-          <IconTrophy />
-        </span>
-        <span className="font-bold text-4xl">124</span>
-        <span className="font-medium text-xs text-[#747DA4]">Campeonatos</span>
-      </div>
-
-      <div className="flex flex-col items-center justify-center min-w-[100px]">
-        <span>
-          <IconUser />
-        </span>
-        <span className="font-bold text-4xl">2.6M</span>
-        <span className="font-medium text-xs text-[#747DA4]">Seguidores</span>
-      </div>
-
-      <div className="flex flex-col items-center justify-center min-w-[100px]">
-        <span>
-          <IconDol />
-        </span>
-        <span className="font-bold text-4xl">14M</span>
-        <span className="font-medium text-xs text-[#747DA4]">
-          Recompensados
-        </span>
-      </div>
+      {cardContent.map(({ icon, number, title }, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center justify-center min-w-[100px]"
+        >
+          <span>{icon}</span>
+          <span className="font-bold text-4xl">{number}</span>
+          <span className="font-medium text-xs text-[#747DA4]">{title}</span>
+        </div>
+      ))}
     </div>
   );
 }
